33bbec7902022ab30006386208f193dd11767303
Wilbeibi <wilbeibi@gmail.com>
2015-09-18 23:50:21 -0700

    :kissing_closed_eyes:

--- CPP/word_ladder.cc
+++ CPP/word_ladder.cc
@@ -3,7 +3,44 @@
 #include <vector>
 using namespace std;
 
-
+class Solution {
+    
+private:
+    vector<string> find_next(string curr, unordered_set<string> &dict) {
+        vector<string> res;
+        for (int i = 0; i < curr.size(); i++) {
+            char tmp = curr[i];
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (c == tmp)
+                    continue;
+                curr[i] = c;
+                if (dict.count(curr)) {
+                    res.push_back(curr);
+                    dict.erase(curr);
+                }
+            }
+            curr[i] = tmp;
+        }
+        return res;
+    }
+public:
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
+        queue<pair<string, int>> q;
+        q.push(make_pair(beginWord, 1));
+        while (!q.empty()) {
+            string curr = q.front().first;
+            int count = q.front().second;
+            if (curr == endWord)
+                return count;
+            q.pop();
+            vector<string> next_words = find_next(curr, wordList);
+            for (auto word: next_words) {
+                q.push(make_pair(word, count+1));
+            }
+        }
+        return 0;
+    }
+};
 
 int main(int argc, char *argv[])
 {
--- RealBattles/Move0And1.cc
+++ RealBattles/Move0And1.cc
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+void move01() {
     //int arr[] = {1,0,0,1,0,1,0,0,1,0};
     int arr[] = {1,1,1,1,1,0};
     int left = 0, right = sizeof(arr)/sizeof(int) - 1;
@@ -17,7 +16,15 @@ int main(int argc, char *argv[])
 	    swap(arr[left], arr[right]);
     }
     for (int i = 0; i < sizeof(arr)/sizeof(int); i++) {
-	cout<<arr[i]<<endl;
+	cout<<arr[i];
     }
+    cout<<endl;
+}
+
+
+
+int main(int argc, char *argv[]) {
+    move01();
+
     return 0;
 }
